8fb7b48945bbf02aea9083538f588c2eb76ea969
kmurata <muratapp@gmail.com>
2025-01-15 23:32:15 +0800

    不要ファイルの消去

---
+++ SwipeCards/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  SwipeCards
+//
+//  Created by 村田興児 on 2025/01/14.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
